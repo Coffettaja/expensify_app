45bc83697270072b6da07e4ffecabbc5af8e8f94
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-10-31 21:47:39 +0200

    404 page

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './styles/styles.sass'
 import 'normalize.css/normalize.css'
 
@@ -28,17 +28,24 @@ const HelpPage = () => (
   </div>
 )
 
+const NotFoundPage = () => (
+  <div>
+    404!!!
+  </div>
+)
+
 // BrowserRouter expects just a single element inside (in this case div)
 // When Route path is matched, the component is rendered
 // 'exact' states that the route path has to be exactly that
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}></Route>
       <Route path="/create" component={AddExpensePage} ></Route>
       <Route path="/edit" component={EditExpensePage} ></Route>
       <Route path="/help" component={HelpPage} ></Route>
-    </div>
+      <Route component={NotFoundPage}></Route>
+    </Switch>
   </BrowserRouter>
 )
 
