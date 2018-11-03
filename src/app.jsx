fa9685fdb8e158c57e4919cbf6d7259373825cda
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-03 21:54:35 +0200

    improving the readability of getVisibleExpenses

--- src/app.jsx
+++ src/app.jsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter.jsx'
 import configureStore from './store/configureStore.jsx'
 import { addExpense } from './actions/expenses.jsx'
-import { setTextFilter } from './actions/filters.jsx'
+import { setTextFilter, sortByAmount, sortByDate } from './actions/filters.jsx'
 import getVisibleExpenses from './selectors/expenses.jsx'
 import './styles/styles.sass'
 import 'normalize.css/normalize.css'
 
 const store = configureStore()
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4000}))
-store.dispatch(addExpense({description: 'Gas bill', amount: 2000}))
+store.dispatch(addExpense({description: 'Wata bill', amount: 40, createdAt: 1}))
+store.dispatch(addExpense({description: 'Gas bill', amount: 20, createdAt: 2}))
 console.log(store.getState())
-store.dispatch(setTextFilter('water'))
+// store.dispatch(setTextFilter('wa'))
+store.dispatch(sortByAmount())
+store.dispatch(sortByDate())
 
 const visibleExpenses = getVisibleExpenses(store.getState().expenses, store.getState().filters)
 
