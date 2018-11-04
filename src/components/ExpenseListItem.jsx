61883d368b345306d0f5a924279114fca1a8e440
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-04 23:41:47 +0200

    filtering by text and deleting expenses from the store

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { removeExpense } from '../actions/expenses.jsx'
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
-  <li> {description}
-    <ul>
-      <li>{amount}</li>
-      <li>{createdAt}</li>
-    </ul>
-  </li>
+const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => (
+  <div>
+    <li> {description}
+      <ul>
+        <li>{amount}</li>
+        <li>{createdAt}</li>
+      </ul>
+    </li> 
+    <button onClick={(e) => {
+      dispatch(removeExpense(id)) 
+    }}>Remove!</button>
+  </div>
 )
 
-export default ExpenseListItem
+// Connecting without param still gives the component access to dispatch() in props.
+export default connect()(ExpenseListItem)
