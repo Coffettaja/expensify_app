86353882f481da69edaf718ea6894808248e2108
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-07 18:40:16 +0200

    nesting test cases with describe

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,49 +1,60 @@
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../../actions/filters.jsx'
 import moment from 'moment'
 
-test('should generate set text filter action with default values', () => {
-  const action = setTextFilter()
-  expect(action).toEqual({
-    type: 'SET_TEXT_FILTER',
-    text: ''
+
+describe('setTextFilter', () => {
+  test('should generate set text filter action with default values', () => {
+    const action = setTextFilter()
+    expect(action).toEqual({
+      type: 'SET_TEXT_FILTER',
+      text: ''
+    })
+  })
+
+  test('should generate set text filter action object with provided values', () => {
+    const text = 'test text'
+    const action = setTextFilter(text)
+    expect(action).toEqual({
+      type: 'SET_TEXT_FILTER',
+      text: text
+    })
   })
 })
 
-test('should generate set text filter action object with provided values', () => {
-  const text = 'test text'
-  const action = setTextFilter(text)
-  expect(action).toEqual({
-    type: 'SET_TEXT_FILTER',
-    text: text
+describe('sortByAmount', () => {
+  test('should generate sort by amount action object', () => {
+    const action = sortByAmount()
+    expect(action).toEqual({
+      type: 'SORT_BY_AMOUNT'
+    })
   })
 })
 
-test('should generate sort by amount action object', () => {
-  const action = sortByAmount()
-  expect(action).toEqual({
-    type: 'SORT_BY_AMOUNT'
+describe('sortByDate', () => {
+  test('should generate sort by date action object', () => {
+    const action = sortByDate()
+    expect(action).toEqual({
+      type: 'SORT_BY_DATE'
+    })
   })
 })
 
-test('should generate sort by date action object', () => {
-  const action = sortByDate()
-  expect(action).toEqual({
-    type: 'SORT_BY_DATE'
+describe('setStartDate', () => {
+  test('should generate set start date action object', () => {
+    const action = setStartDate(moment(0))
+    expect(action).toEqual({
+      type: 'SET_START_DATE',
+      date: moment(0)
+    })
   })
 })
 
-test('should generate set start date action object', () => {
-  const action = setStartDate(moment(0))
-  expect(action).toEqual({
-    type: 'SET_START_DATE',
-    date: moment(0)
-  })
-})
-
-test('should generate set end date action object', () => {
-  const action = setEndDate(moment(0))
-  expect(action).toEqual({
-    type: 'SET_END_DATE',
-    date: moment(0)
+describe('setEndDate', () => {
+  test('should generate set end date action object', () => {
+    const action = setEndDate(moment(0))
+    expect(action).toEqual({
+      type: 'SET_END_DATE',
+      date: moment(0)
+    })
   })
 })
