fec817f4d367b7271ab2b8a5bad72e1dc637b5a0
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-13 00:32:49 +0200

    separating css files from the js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+// extracts text from the bundle.js to its own file
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // On this object, all the configuration details for webpack build are defined.
 // module.exports is a way to expose something to other files.
 module.exports = (env) => {
   const isProduction = env === 'production'
+  // argument is the filename of the extracted text
+  const CSSExtract = new ExtractTextPlugin('styles.css')
 
   return {
     entry: './src/app.js',
@@ -20,15 +24,29 @@ module.exports = (env) => {
         exclude: /node_modules/ // Exclude the files in node_modules folder
       }, {
         test: /\.(sass|css)$/,
-        use: [ // Array of loaders
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader:'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
+    plugins: [
+      CSSExtract
+    ],
     // 'source-map' takes a lot more time to build, but it only loads when the dev tools are opened, so it does not slow down normal visitors
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true // tells the devServer that the routing is handled client-side, and therefore the index.html page should be served for all 404 routes
