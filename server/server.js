9c2a1452908b08c12ad9efe543c4e7bbb22604f6
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-14 18:43:57 +0200

    setting up for heroku deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
+const port = process.env.PORT || 3000 // set by Heroku
 
 // Serves up all assets from the public directory
 app.use(express.static(publicPath))
@@ -12,6 +13,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running!')
 })
