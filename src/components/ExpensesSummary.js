8bdbd9386298ae8f5b2f752ff52f201c29f7f694
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-15 23:51:59 +0200

    added text for summary component even when at 0 expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,11 +5,11 @@ import getVisibleExpenses from '../selectors/expenses'
 import formatAmount from '../selectors/formatAmount'
 
 export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
-  if (expensesCount === 0) return null
+  // if (expensesCount === 0) return null // whole page shifts when filtering...
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses'
   return (
     <div>
-      <p>Viewing {expensesCount} {expenseWord}, totalling {formatAmount(expensesTotal)}</p>
+      {expensesCount === 0 ? <h3>No expenses</h3> : <h3>Viewing {expensesCount} {expenseWord}, totalling {formatAmount(expensesTotal)}</h3>}
     </div>
   )
 }
