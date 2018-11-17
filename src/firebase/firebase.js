2448204426f9d5109ce4bfe358d3081583bcbd4e
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-17 23:07:24 +0200

    playing around with firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,8 +9,23 @@ const config = {
     messagingSenderId: "379793181329"
   }
 
-  firebase.initializeApp(config)
+firebase.initializeApp(config)
 
-  firebase.database().ref().set({
-    name: "testi Joonas"
-  })
+const database = firebase.database()
+
+// .ref() gives a reference to a specific part in the database
+// .set() sets the database value for that part. It overrides any previous values.
+// therefore, to update a value, a parameter for ref() has to be given
+// e.g. .ref('name').set('some other name')
+// referencing values inside an object done by using / within the string value
+// database.ref().set({
+//   name: "testi Joonas",
+//   age: 26,
+//   isFinnish: true
+// }).then(() => {
+//   console.log('data was saved')
+// }).catch((e) => {
+//   console.log('error:', e)
+// })
+
+database.ref('isFinnish').remove()
