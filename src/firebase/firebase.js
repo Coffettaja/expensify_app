53acb2063eb93e3071bfad9d3b77154f88fee628
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-21 13:52:09 +0200

    setting up a separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyCz9KrGfau4XwRSuLKGyIiNFcZsM7bJV9g",
-    authDomain: "expense-manager-61834.firebaseapp.com",
-    databaseURL: "https://expense-manager-61834.firebaseio.com",
-    projectId: "expense-manager-61834",
-    storageBucket: "expense-manager-61834.appspot.com",
-    messagingSenderId: "379793181329"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   }
 
 firebase.initializeApp(config)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
-// extracts text from the bundle.js to its own file
+const webpack = require('webpack')
+// extracts text from the bundle.js to its own file ... used for css mostly
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+
+// Heroku sets automatically to 'production'
+// 'test' = test environment, undefined = development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development' // Stores the enviroment
+
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' })
+}
 
 // On this object, all the configuration details for webpack build are defined.
 // module.exports is a way to expose something to other files.
@@ -43,7 +53,16 @@ module.exports = (env) => {
       }]
     },
     plugins: [
-      CSSExtract
+      CSSExtract,
+      // Need to use stringify here so actually the string is used in the firebase config and not just the text inside (as in now the result is apiKey: "value", instead of apiKey: value)
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+      })
     ],
     // 'source-map' takes a lot more time to build, but it only loads when the dev tools are opened, so it does not slow down normal visitors
     devtool: isProduction ? 'source-map' : 'inline-source-map',
