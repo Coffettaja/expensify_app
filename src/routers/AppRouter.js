087a358f52ab19e89c7fe5a1c057ede282ed7389
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-24 12:38:00 +0200

    redirecting with login status change

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { Route, Router, Switch, Link, NavLink } from 'react-router-dom'
 import AddExpensePage from '../components/AddExpensePage'
+import createHistory from 'history/createBrowserHistory'
 import EditExpensePage from '../components/EditExpensePage'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
 import Header from '../components/Header'
@@ -8,11 +9,14 @@ import HelpPage from '../components/HelpPage'
 import LoginPage from '../components/LoginPage'
 import NotFoundPage from '../components/NotFoundPage'
 
+export const history = createHistory()
+
 const AppRouter = () => (
   // BrowserRouter expects just a single element inside (in this case div)
   // When Route path is matched, the component is rendered
   // 'exact' states that the route path has to be exactly that
-  <BrowserRouter>
+  // BrowserRouter replaced with just plain Router so custom history value can be passed in
+  <Router history={history}>
     <div>
       <Header></Header> 
       <Switch>
@@ -24,7 +28,7 @@ const AppRouter = () => (
         <Route component={NotFoundPage}></Route>
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 )
 
 export default AppRouter
