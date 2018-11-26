85f6a22dfd16e59daf39c7a1894d838b63f9b790
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-11-26 20:46:57 +0200

    styling summary area

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import getExpensesTotalAmount from '../selectors/getExpensesTotalAmount'
 import getVisibleExpenses from '../selectors/expenses'
 import formatAmount from '../selectors/formatAmount'
@@ -8,8 +9,13 @@ export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
   // if (expensesCount === 0) return null // whole page shifts when filtering...
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses'
   return (
-    <div>
-      {expensesCount === 0 ? <h3>No expenses</h3> : <h3>Viewing {expensesCount} {expenseWord}, totalling {formatAmount(expensesTotal)}</h3>}
+    <div className="page-header">
+      <div className="content-container">
+        {expensesCount === 0 ? <h1 className="page-header__title">No expenses</h1> : <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord}, totalling <span>{formatAmount(expensesTotal)}</span></h1>}
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   )
 }
