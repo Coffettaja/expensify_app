5aa49a5c32a749d13edbe9c4fb2dd28ab5aae245
Joonas Karttunen <joonas.v.a.karttunen@student.jyu.fi>
2018-12-07 23:30:39 +0200

    rewrote expense summary wording

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,12 +7,20 @@ import formatAmount from '../selectors/formatAmount'
 
 export const ExpensesSummary = ({visibleExpensesCount, totalExpensesCount, expensesTotal}) => {
   const viewingAllExpenses = visibleExpensesCount === totalExpensesCount
+  const noExpenses = totalExpensesCount === 0
+  const noVisibleExpenses = visibleExpensesCount === 0
   return (
     <div className="page-header">
       <div className="content-container">
-        {viewingAllExpenses ? 
-          <h1 className="page-header__title">Viewing all <span>{visibleExpensesCount}</span> expenses, totalling <span>{formatAmount(expensesTotal)}</span></h1> : 
-          <h1 className="page-header__title">Viewing <span>{visibleExpensesCount}</span> ouf of {totalExpensesCount} expenses, totalling <span>{formatAmount(expensesTotal)}</span></h1>}
+        {
+        noExpenses ? 
+          <h1 className="page-header__title">No expenses.</h1> : 
+        viewingAllExpenses ? 
+          <h1 className="page-header__title">Viewing all <span>{visibleExpensesCount}</span> expenses, totalling <span>{formatAmount(expensesTotal)}</span></h1> :
+        noVisibleExpenses ? 
+          <h1 className="page-header__title">Viewing no expenses ouf of {totalExpensesCount} expenses.</h1> :
+          <h1 className="page-header__title">Viewing <span>{visibleExpensesCount}</span> ouf of {totalExpensesCount} expenses, totalling <span>{formatAmount(expensesTotal)}</span></h1>
+        }
         <div className="page-header__actions">
           <Link className="button" to="/create">Add Expense</Link>
         </div>
